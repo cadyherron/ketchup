8cfdf1695db6d407865c5fa62e0be8f04720543d
Julia Herron <julia.herron@gmail.com>
2016-04-02 16:46:22 -0700

    started README

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # ketchup
+
+
+#### WHAT IS THIS?
+
+Ketchup is a service to mirror all your hidden commits to your Github commit history.
+
+
+#### WHY SHOULD I USE THIS?
+
+Let's say you're enrolled in a program where you fork a repository, build an app, and submit a pull request. That pull request will never be accepted. But you still want all your hard work to show up in those encouraging little green boxes on your Github profile. Enter... ketchup.
+
+
+#### HOW DO I RUN THIS THING?
+
+Step 1: Generate a personal access token through Github
+* Go to your settings page (the gear icon in your navbar).
+* On the left side, click the "Personal access tokens" tab.
+* Generate a new access token with permissions for "public repo", "repo status", and "notifications".
+* Copy down that token.
+
+Step 2: Clone this repo
+
+Step 3: Run the following from the command line:
+
+`k = Ketchup.new('github_username', 'github_api_key')`
+`k.run`
---
+++ ketchup.rb
@@ -0,0 +1,124 @@
+require 'github_api'
+
+
+class Ketchup
+
+  def initialize(username, access_token)
+    @oauth_token = access_token
+    @github = Github.new(oauth_token: @oauth_token)
+    @username = username
+  end
+
+
+  def create_repo
+    @github.repos.create "name": 'ketchup-repo',
+    "description": "This is your ketchup repo",
+      "homepage": "https://github.com",
+      "private": false,
+      "has_issues": true,
+      "has_wiki": true,
+      "has_downloads": true
+  end
+
+
+  def find_ketchup_repo
+    begin
+      @github.repos(user: @username, repo: 'ketchup-repo').get
+    rescue Github::Error::NotFound
+      false
+    end
+  end
+
+
+  def ketchup_repo
+    find_ketchup_repo || create_repo
+  end
+
+
+  def get_commits(repo)
+    @github.repos(user: @username, repo: repo).commits.list.map { |commit|
+       {date: commit.commit.committer.date,
+        name: commit.commit.message }
+     }
+  end
+
+
+  def clone_ketchup
+    `git clone #{ketchup_repo.ssh_url}`
+  end
+
+
+  def update_readme(date, repo)
+    File.open('README', 'a+') do |file|
+      file.write "#{date} Mirror commit msg for #{repo}\n"
+    end
+  end
+
+
+  def fake_commit(date)
+    `git add .`
+    `GIT_AUTHOR_DATE='#{date}' GIT_COMMITTER_DATE='#{date}' git commit -m 'Mirrored commit'`
+  end
+
+
+  def push
+    # set upstream version of ketchup-repo
+    Dir.chdir('ketchup-repo') do
+      `git push -u origin master`
+    end
+  end
+
+
+  def select_forks
+    selected_forks = forks.map do |fork|
+      puts "Would you like to ketchup #{fork}? (y/n)"
+      answer = gets.chomp
+      case answer
+      when /^[Yy]/
+        fork
+      else
+        nil
+      end
+    end.compact
+    selected_forks
+  end
+
+
+  def remove_ketchup_folder
+    `rm -rf ketchup-repo`
+  end
+
+
+  def ketchup_history(repo)
+    Dir.chdir('ketchup-repo') do
+      # loop through original commits
+      get_commits(repo).reverse.each do |commit|
+        date = commit[:date]
+        update_readme(date, repo)
+        fake_commit(date)
+      end
+    end
+  end
+
+
+  def forks
+    forked_repos = @github.repos.list.select(&:fork).map(&:name)
+  end
+
+
+  def run
+    selected_forks = select_forks
+    p selected_forks
+    # clone_ketchup
+    # selected_forks.each do |fork|
+    #   ketchup_history(fork)
+    # end
+    # push
+    # remove_ketchup_folder
+  end
+
+  
+end
+
+k = Ketchup.new('cadyherron', '95f57933faf9dac7f055105e8aa092689e82380b')
+k.run
